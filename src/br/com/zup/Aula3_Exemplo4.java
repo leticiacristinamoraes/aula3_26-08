2ddebc4d25a80d420df18640017416898ffe236c
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-26 13:14:50 -0300

    Exercício 4 funcionando

---
+++ src/br/com/zup/Aula3_Exemplo4.java
@@ -0,0 +1,30 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Aula3_Exemplo4 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Por favor, digite a primeira nota:");
+        double nota1 = leitor.nextDouble();
+        System.out.println("Por favor, digite a segunda nota:");
+        double nota2 = leitor.nextDouble();
+
+        // Calculando a média
+        double media = (nota1 + nota2 / 2);
+
+        if (media >= 0 & media < 7) {
+            System.out.println("Você foi reprovado com a nota:" + media);
+        }
+        else if (media >= 7 & media < 10) {
+            System.out.println("Você foi aprovado com a nota:" + media);
+        }
+        else if (media == 10) {
+            System.out.println("Você foi aprovado com distinção! Parabéns pelo desempenho!!!");
+        }
+        else{
+            System.out.println("Por favor, digite um valor válido!");
+        }
+    }
+}
